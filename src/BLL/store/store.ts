2666ee2de2d2aab24b54ff02f225374bf2827a53
0Ieg <obognov@gmail.com>
2023-10-13 22:51:02 +0300

    Adding a Business Logic Layer

--- src/BLL/store/store.ts
+++ src/BLL/store/store.ts
@@ -1,1 +1,8 @@
-import { aboutUsReducer } from "./aboutUsReducer";
+import { aboutUsReducer } from "./aboutUsSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer:{
+    aboutUs: aboutUsReducer,
+  }
+})
