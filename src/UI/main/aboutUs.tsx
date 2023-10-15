5d5ea7b10bf439954b07ba7fbe31f3066f26a570
0Ieg <obognov@gmail.com>
2023-10-15 12:21:03 +0300

    Completing the creation of the About US section

--- src/UI/main/aboutUs.tsx
+++ src/UI/main/aboutUs.tsx
@@ -2,9 +2,6 @@ import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Company } from '../templates/companyInformation';
-import vg from '../../BLL/images/vg.png'
-import bo from '../../BLL/images/bo.png'
-import sx from '../../BLL/images/sx.png'
 import { StateType } from '../../BLL/store/store';
 
 const AboutUsS = styled.section`
@@ -14,38 +11,11 @@ const AboutUsS = styled.section`
   color: var(--color-white);
   padding: 96px 0 48px;
 }
+& article{
+  padding-bottom: 150px;
+}
 `
 
-const virginGalactic = {
-  name: 'Virgin Galactic',
-  achievement: 'суборбитальный полет',
-  news:{
-    new1: 'Компания Virgin Galactic 11 июля 2021 осуществила первый "туристический" суборбитальный полет со своим основателем, британским миллиардером Ричардом Брэнсоном на борту.',
-    new2: 'Корабль "Unity 22" стартовал с космического порта "Америка" в штате Нью-Мексико с помощью самолета-носителя VMS Eve. Позже он отсоединился от самолета-носителя.',
-    new3: 'Руководство Virgin Galactic надеется, что массовые суборбитальные космические полеты начнутся до 2022 года. ',
-  },
-  img: vg,
-}
-const blueOrigin = {
-  name: 'Blue Origin',
-  achievement: 'суборбитальный полет',
-  news:{
-    new1: 'Суборбитальные ракета New Shepard компании Blue Origin 20 июля 2021года впервые в истории компании вывела на космическую высоту капсулу с людьми, с самым богатым человеком мира Джеффом  Безосом на борту.',
-    new2: 'New Shepard высотой 18 м и шириной 4 м - многоразовый космический аппарат. Бустерная установка поднимает герметичное пассажирское судно на высоту около 76 км. Импульс преподносит капсулу вверх в космос. ',
-    new3: 'После этого она опускается на землю под тремя парашютами. Ракета-носитель тоже возвращается домой.',
-  },
-  img: bo,
-}
-const spaceX = {
-  name: 'SpaceX',
-  achievement: 'А может на Луну или Марс?',
-  news:{
-    new1: 'В 2016 году на 67-м ежегодном Международном конгрессе астронавтики Илон Маск заявил, что оптимистичный временной график предусматривает первый полет к Марсу в 2022 году (доставка груза).',
-    new2: 'Полет с экипажем - в 2024 году. На Красную планету экипаж прибудет в 2025-26 году."Я начал SpaceX, потому что хотел помочь снизить стоимость космических перевозок, чтобы основать жизни на другой планете".',
-    new3: 'Если все пойдет по плану, то благодаря Маск к 2050 году жителями Красной планеты смогут себя назвать около миллиона человек. Туда их доставит флотилия из тысячи кораблей Starship.',
-  },
-  img: sx,
-}
 export const AboutUs: FC = () => {
   const companies = useSelector((state:StateType)=>state.aboutUs)
   return (
--- src/UI/templates/companyInformation.tsx
+++ src/UI/templates/companyInformation.tsx
@@ -1,14 +1,13 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 import { CompanyType } from '../../BLL/store/aboutUsSlice';
+import { Button } from './button';
 
 const CompanyS = styled.article`
 color: var(--color-white);
 display: flex;
+gap: 20px;
 justify-content: space-between;
-&>*:not(:last-child){
-    margin-bottom: 150px;
-}
 .information{
   max-width: 600px;
   width: 100%;
@@ -30,6 +29,36 @@ justify-content: space-between;
       max-width: 165px;
     }
   }
+  .table{
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 50px;
+    .thead{
+      border-top: 2px solid var(--color-white);
+      border-bottom: 2px solid var(--color-white);
+      tr{
+        font: 400 16px SFPro;
+        th{
+          padding: 10px 0;
+        }
+      }
+    }
+    .tbody{
+      border-bottom: 2px solid var(--color-white);
+      td{
+        padding: 2px 0 8px;
+        div{
+          text-align: center;
+          &:nth-child(1){
+            font: 700 36px SFPro;
+          }
+          &:nth-child(2){
+            font: 700 14px SFPro;
+          }
+        }
+      }
+    }
+  }
 }
 `
 
@@ -45,6 +74,42 @@ export const Company:FC<CompanyType> = (props) => {
           <div className="new">{props.news.new2}</div>
           <div className="new">{props.news.new3}</div>
         </div>
+        <table className='table'>
+          <thead className='thead'>
+            <tr>
+              <th>Время в пути</th>
+              <th>Расстояние</th>
+              <th>В невесомости</th>
+              <th>Стоимость</th>
+              <th>Вернешься?</th>
+            </tr>
+          </thead>
+          <tbody className='tbody'>
+            <tr>
+              <td>
+                <div>{props.tableData.travel[0]}</div>
+                <div>{props.tableData.travel[1]}</div>
+              </td>
+              <td>
+                <div>{props.tableData.distance[0]}</div>
+                <div>{props.tableData.distance[1]}</div>
+              </td>
+              <td>
+                <div>{props.tableData.weightlessness[0]}</div>
+                <div>{props.tableData.weightlessness[1]}</div>
+              </td>
+              <td>
+                <div>{props.tableData.price[0]}</div>
+                <div>{props.tableData.price[1]}</div>
+              </td>
+              <td>
+                <div>{props.tableData.chance[0]}</div>
+                <div>{props.tableData.chance[1]}</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <Button size='small' title='ЗАБРОНИРОВАТЬ ПОЛЕТ'/>
       </div>
     </CompanyS>
   )
