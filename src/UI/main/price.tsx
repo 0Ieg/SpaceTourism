0ed6045ff35d117680965b9c11101e0fc1579e5a
0Ieg <obognov@gmail.com>
2023-10-21 12:27:38 +0300

    Minor changes

--- src/UI/main/price.tsx
+++ src/UI/main/price.tsx
@@ -14,21 +14,26 @@ color: var(--color-white);
 }
 .offers{
   display: grid;
-  grid-template-columns: repeat(2, max-content);
+  grid-template-columns: repeat(2, 500px);
   justify-content: space-between;
+  gap: 25px;
   .offer{
     display: flex;
     flex-direction: column;
     gap: 25px;
     .content{
       height: 400px;
-      max-width: 500px;
+      padding: 40px 75px;
       border: 1px solid var(--color-white);
       border-radius: 5px;
       box-shadow: inset 0px 0px 10px #fff, 0px 0px 5px #fff;
       background-color: hsla(0, 0%, 100%, 2%);
+      display: grid;
+      justify-items: center;
       .text{
         font: 400 24px SFPro;
+        width: 236px;
+        text-align: center;
         .red{
           color: var(--color-red);
         }
@@ -37,6 +42,7 @@ color: var(--color-white);
     .old_price{
       font: 400 16px SFPro;
       position: relative;
+      justify-self: end;
       &::after{
         content: '';
         width: 100%;
@@ -44,7 +50,7 @@ color: var(--color-white);
         background-color: var(--color-red);
         position: absolute;
         z-index: 3;
-        top: 50%;
+        top: 8px;
         left: 0;
         transform: rotate(15deg) translateY(-50%);
       }
@@ -65,7 +71,7 @@ export const Price: FC = () => {
       <div className="offers">
         <div className="offer">
         <div className="content">
-            <div className="text">Забронируй полет <span className='red'>прямо сейчас</span> и получи скидку 1000$</div>
+            <div className="text">Забронируй полет <span className='red'>прямо сейчас</span> и получи скидку <br/>1000$</div>
             <span className="old_price">250 000 $</span>
             <div className="new_price">249 000 $</div>
           </div>
