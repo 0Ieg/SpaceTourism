3cfbc518ba9c9bf64ee4c697d0036ed01ddfd9ed
0Ieg <obognov@gmail.com>
2023-10-25 22:04:09 +0300

    Modification of the slider

--- src/UI/templates/slider.tsx
+++ src/UI/templates/slider.tsx
@@ -1,5 +1,8 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
+import photo1 from "../../BLL/images/slides/1.jpg"
+import photo2 from "../../BLL/images/slides/2.jpg"
+import photo3 from "../../BLL/images/slides/1.jpg"
 
 const Styled = styled.div`
 width: 100%;
@@ -12,11 +15,22 @@ border-radius: 5px;
 display: grid;
 grid-template-columns: max-content 1fr max-content;
 align-items: center;
-
 .window{
   width: 100%;
   height: 100%;;
   box-shadow: inset 0px 0px 10px #fff, 0px 0px 5px #fff;
+  overflow: hidden;
+  .slides{
+    display: flex;
+    height: 100%;
+    width: 100%;
+    transition: transform 1s ease;
+    .slide{
+      flex-shrink: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 .previous, .next{
   cursor: pointer;
@@ -40,11 +54,14 @@ align-items: center;
 }
 `
 export const Slider:FC = ()=>{
+  const photos = [photo1,photo2,photo3]
+  const Slides = photos.map((photo,index)=><img src={photo} alt="slide" className="slide" key={index}/>)
+  const [value, setValue] = useState(0)
   const previousHandler = ()=>{
-    console.log('previous');
+    value>=1 && setValue(value-1)
   }
   const nextHandler = ()=>{
-    console.log('next');
+    value<photos.length-1 && setValue(value+1)
   }
   return(
     <Styled>
@@ -53,7 +70,11 @@ export const Slider:FC = ()=>{
           <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
         </svg>
       </button>
-      <div className="window"></div>
+      <div className="window">
+        <div className="slides" style={{transform:`translateX(-${value*100}%)`}}>
+          {Slides}
+        </div>
+      </div>
       <button className="next" onClick={nextHandler}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
